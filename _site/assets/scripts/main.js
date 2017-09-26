00bfcfb756d488913186e2df3b227f92914f8cea
@henryjr <hmjsite@gmail.com>
2017-09-26 10:59:19 -0300

    Base BugOS
    Signed-off-by: @henryjr <hmjsite@gmail.com>

--- _site/assets/scripts/main.js
+++ _site/assets/scripts/main.js
@@ -18,16 +18,16 @@ $.fn.descendantOf = function(element) {
 $('.menu-nojs').hide();
 
 // Asyn load images
-document.write('<img style="display: none" src="http://localhost:4000//assets/images/patterns/dark-fabric.png">');
-document.write('<img style="display: none" src="http://localhost:4000//assets/images/patterns/dark-fabric.png">');
+document.write('<img style="display: none" src="http://localhost:4000/assets/images/patterns/dark-fabric.png">');
+document.write('<img style="display: none" src="http://localhost:4000/assets/images/patterns/dark-fabric.png">');
 
 // Load theme dark-light
 if (typeof(Storage) !== "undefined") {
     if (sessionStorage.theme == "dark") {
-        $('body').css("background", "url(http://localhost:4000//assets/images/patterns/dark-fabric.png)");
+        $('body').css("background", "url(http://localhost:4000/assets/images/patterns/dark-fabric.png)");
         $('input#theme-select').prop("checked", true);
     } else if (sessionStorage.theme == "dark") {
-        $('body').css("background", "url(http://localhost:4000//assets/images/patterns/dark-fabric.png)");
+        $('body').css("background", "url(http://localhost:4000/assets/images/patterns/dark-fabric.png)");
         $('input#theme-select').prop("checked", false);
         sessionStorage.theme = "light";
     }
@@ -39,10 +39,10 @@ $(document).ready(function(){
     // Theme selector
     $('input#theme-select').change(function(){
         if ($('input#theme-select').is(':checked')) {
-            $('body').css("background", "url(http://localhost:4000//assets/images/patterns/dark-fabric.png)");
+            $('body').css("background", "url(http://localhost:4000/assets/images/patterns/dark-fabric.png)");
             sessionStorage.theme = "dark";
         } else {
-            $('body').css("background", "url(http://localhost:4000//assets/images/patterns/seigaiha.png)");
+            $('body').css("background", "url(http://localhost:4000/assets/images/patterns/seigaiha.png)");
             sessionStorage.theme = "light";
         }
     });
